972cb4925b847c6c0fae396d8c03bb989c263195
JCS PRO <JCS-PRO@iMac-de-JCS.local>
2014-12-08 15:51:39 +0100

    installation

---
+++ src/raf.js
@@ -0,0 +1,24 @@
+(function(window, document, undefined) {
+    'use strict';
+
+    window.requestAnimFrame = (function(){
+        return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function(/* function */ callback, /* DOMElement */ element){
+                return window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    window.cancelRequestAnimFrame = ( function() {
+        return window.cancelAnimationFrame          ||
+            window.webkitCancelRequestAnimationFrame    ||
+            window.mozCancelRequestAnimationFrame       ||
+            window.oCancelRequestAnimationFrame     ||
+            window.msCancelRequestAnimationFrame        ||
+            clearTimeout
+    } )();
+
+}(window, document));
